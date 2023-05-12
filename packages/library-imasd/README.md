e83a18d2aa2845a209bf79de14f1a4999cba5140
Rafael Chardi <rafaelchardi@gmail.com>
2023-05-12 13:41:58 +0200

    primer comit

---
+++ packages/library-imasd/README.md
@@ -0,0 +1,7 @@
+# library-imasd
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test library-imasd` to execute the unit tests.
---
+++ packages/web-imasd/src/app/app.module.ts
@@ -0,0 +1,37 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [ HttpClient ]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { 
+  constructor(private translate: TranslateService){
+    this.translate.setDefaultLang('es');
+  }
+}
